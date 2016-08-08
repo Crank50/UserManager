67fde5759ad4dc00cfa15b867292035deac89023
Justin <Crank50@icloud.com>
2016-08-08 09:36:47 -0600

    Initial commit

---
+++ src/main/java/springBootApp/Application.java
@@ -0,0 +1,15 @@
+package springBootApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Justin on 8/8/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
